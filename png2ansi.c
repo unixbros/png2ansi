7f55ef806617c77c909481c7da5ab0ecf1497fef
dcat <dcat@iotek.org>
2016-11-26 03:06:00 +0100

    make it work

--- png2ansi.c
+++ png2ansi.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <err.h>
 
 #include "upng.h"
@@ -9,20 +10,26 @@ main(int argc, char **argv) {
 	upng_t *png;
 	char *path = argv[1];
 	unsigned int color;
-	int i, max;
+	unsigned int i, x, max;
 
 	png = upng_new_from_file(path);
 	if (png != NULL) {
 		upng_decode(png);
 		if (upng_get_error(png) == UPNG_EOK) {
-			max = (upng_get_width(png) * upng_get_height(png)) * 3;
+			max = upng_get_size(png);
 
-			for (i = max; i; i -= 4) {
+			for (i = 0; i < max; i += upng_get_components(png)) {
 				color = rgb2hex(upng_get_buffer(png)[i],
 						upng_get_buffer(png)[i+1],
 						upng_get_buffer(png)[i+2]);
-			}
 
+				printf("\033[48;5;%dm \033[0m",
+						closest(color));
+
+				x++;
+				if ((x % upng_get_width(png) == 0))
+					putchar('\n');
+			}
 		}
 
 		upng_free(png);
